7a90a5a9c8f08e41f8b2d734f2503b9183efff67
Noah Feehan <nfeehan@gmail.com>
2014-04-22 18:17:15 -0400

    first commit

---
+++ startAPI.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# listening table - semantic listening enchanting a table
+# (c)nytlabs 2014
+
+import datetime
+import logging
+import os
+from pymongo import MongoClient
+import tornado
+import tornado.ioloop
+import tornado.web
+import tornado.options
+import tornado.template
+
+
+from handlers.FixtureHandler import FixtureHandler
+
+
+
+if __name__ == '__main__':
+
+    tornado.options.define('debug', default=True, type=bool, help=(
+        "Turn on autoreload"
+    ))
+    tornado.options.logging = 'debug'
+    tornado.options.parse_command_line()
+
+    client = MongoClient(tz_aware=True)
+    db = client.listening_table
+    this_dir = os.path.dirname(__file__)
+    static_path = os.path.join(this_dir, 'static')
+    application = tornado.web.Application([
+            (r'/fixture', FixtureHandler),
+
+        ], db=db)
+
+    static_path=static_path
+    debug=tornado.options.options.debug
+
+    logging.info('Listening on http://localhost:80')
+    application.listen(80)
+    tornado.ioloop.IOLoop.instance().start()
