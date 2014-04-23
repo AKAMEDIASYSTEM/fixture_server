9695d001ba68340e347bb746ce7d5cfd8c24b60f
Noah Feehan <nfeehan@gmail.com>
2014-04-23 12:40:37 -0400

    simplifying spark core side of things because it is a dumb little guy

--- handlers/FixtureHandler.py
+++ handlers/FixtureHandler.py
@@ -12,7 +12,7 @@ import ResponseObject
 import requests
 
 class FixtureHandler(BaseHandler):
-    """Accept a URL and timestamp submission; do topic analysis"""
+    """Accept a file (CSV) with accelerometer samples in it"""
     def __init__(self, *args, **kwargs):
         BaseHandler.__init__(self,  *args, **kwargs)  
 
@@ -25,5 +25,36 @@ class FixtureHandler(BaseHandler):
             logging.info('we are authenticated and ready to debug data')
             # timestamp = datetime.datetime.utcnow()
             logging.info(self.request.files['fixture_payload'][0]['body'])
+            for line in self.request.files['fixture_payload'][0]['body'].split('\n'):
+                logging.info(line)
+            ''' here is an example line of payload:
+            1398221223683,-5.5385894775390625,-0.2913665771484375,7.8047637939453125
+            that is: UTC timestamp in millis, X, Y, and Z.
+            X, Y, and Z are in meters per second per second, so when lying on a table, Z=-9.8
+            so in general, each axis can vary from 11 to -11. 
+            Map this to the (0,255) range for RGB Neopixel values.
+
+            for line in fixture_payload[0]['body']:
+                vars = line.split(',')
+                look at the timestamp, vars[0]
+                if the timestamp is more than INTERVAL from last update sent:
+                    # this means we have been motionless for longer than INTERVAL, so we should send (thisTimestamp-lastTimestamp)/INTERVAL duplicate updates
+                    # so the neopixel reports the right amount of motionlessness
+                    updateSpark(last_scaledX, last_scaledY, last_scaledZ)
+                else:
+                    # we are up-to-date, send this one out
+                    scaledX = map(vars[0])
+                    scaledY = map(vars[1])
+                    scaledZ = map(vars[2])
+                    updateSpark(scaledX, scaledY, scaledZ)
+                    last_scaledX = map(vars[0])
+                    last_scaledY = map(vars[1])
+                    last_scaledZ = map(vars[2])
+            '''
         else:
             self.write('Something is wack in FixtureHandler, self.isAuth wasnt True.')
+
+    def mapVals(val, inMin, inMax, outMin, outMax):
+        toRet = float(outMin + (float(outMax - outMin) * (float(val - inMin) / float(inMax - inMin))))
+        # return clamp(toRet, outMin, outMax)
+        return toRet
